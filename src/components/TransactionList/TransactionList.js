05a287e7af948b0fd66818242483e519f8cfed92
Dimmon786 <bezborodko.dmytro@gmail.com>
2025-01-10 09:25:54 +0100

    TransactionList

--- src/components/TransactionList/TransactionList.js
+++ src/components/TransactionList/TransactionList.js
@@ -1,2 +1,33 @@
 import React from 'react';
 import './TransactionList.css';
+
+const TransactionList = ({ transaction, onDeleteTransaction }) => {
+    return (
+        <table className="transaction-list">
+            <thead>
+                <tr>
+                    <th>Besschreibung</th>
+                    <th>Betrag</th>
+                    <th>Datum</th>
+                    <th>Aktionen</th>
+                </tr>
+            </thead>
+            <tbody>
+                {transaction.map((transaction, index) => (
+                    <tr key={index}>
+                        <td>{transaction.text}</td>
+                        <td classNme={transaction.amount < 0 ? 'expense' : 'income'}>
+                            {transaction.amount.toFixed(2)}€
+                        </td>
+                        <td>{new Date(transaction.date).toLocaleDateString()}</td>
+                        <td>
+                            <button onClick={() => onDeleteTransaction(index)}>Löschen</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default TransactionList;
